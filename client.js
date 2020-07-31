721630df14308056e9cbc265c634b5237ab02660
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-07-31 08:25:49 -0600

    Server Fix and Color
    Corrected issues with the socket handling and username generation. added color to the client terminal display. added extra command '/addAdmin' which allows the admin to add other users as admins using the server password. By default the first client to connect is given Admin privileges.

--- client.js
+++ client.js
@@ -1,9 +1,45 @@
 const net = require("net");
-const fs = require("fs");
 
-let check = true;  
+const FgBlue = "\x1b[34m";
+const FgRed = "\x1b[31m";
+const FgGreen = "\x1b[32m";
+const FgYellow = "\x1b[33m";
+const FgMagenta = "\x1b[35m";
+const FgWhite = "\x1b[37m";
+const FgCyan = "\x1b[36m";
 
-const client = net.createConnection({ port: 3050 }, () => {
+function readColors(message) {
+  message = message.toString();
+  if (message.includes("\\x1b[34m") && message.includes("\\x1b[35m")) {
+    let removeMagenta = message.split("\\x1b[35m");
+    let removeBlue = removeMagenta[1].split("\\x1b[34m");
+    let name = removeBlue[0];
+    let content = removeBlue[1];
+    console.log(FgMagenta, name.trim(), FgBlue, content, FgWhite);
+  } else if (message.includes("\\x1b[34m")) {
+    let printToConsole = message.slice(8);
+    console.log(FgBlue, printToConsole.toString(), FgWhite);
+  } else if (message.includes("\\x1b[31m")) {
+    let printToConsole = message.slice(8);
+    console.log(FgRed, printToConsole.toString(), FgWhite);
+  } else if (message.includes("\\x1b[32m")) {
+    let printToConsole = message.slice(8);
+    console.log(FgGreen, printToConsole.toString(), FgWhite);
+  } else if (message.includes("\\x1b[33m")) {
+    let printToConsole = message.slice(8);
+    console.log(FgYellow, printToConsole.toString(), FgWhite);
+  } else if (message.includes("\\x1b[35m")) {
+    let printToConsole = message.slice(8);
+    console.log(FgMagenta, printToConsole.toString(), FgWhite);
+  } else if (message.includes("\\x1b[36m")) {
+    let printToConsole = message.slice(8);
+    console.log(FgCyan, printToConsole.toString(), FgWhite);
+  } else {
+    console.log(message.toString());
+  }
+}
+
+const client = net.createConnection({ port: 3051 }, () => {
   console.clear();
 });
 
@@ -12,16 +48,17 @@ client.on("connect", () => {
 });
 
 client.on("data", (response) => {
-  console.log(`#${response}`);
+  readColors(response);
 });
 
 client.on("end", () => {
+  console.log("Server has been ended. Good bye!");
   process.exit();
 });
 
 process.stdin.on("readable", () => {
   let userInput;
-  while ((userInput = process.stdin.read()) !== null && check) {
+  while ((userInput = process.stdin.read()) !== null) {
     client.write(userInput.toString().trim());
   }
 });
--- server.js
+++ server.js
@@ -2,168 +2,353 @@ const net = require("net");
 const fs = require("fs");
 const chatLog = fs.createWriteStream("./logs/chat-log.txt");
 const serverLog = fs.createWriteStream("./logs/server-log.txt");
+const { v4: uuidv4 } = require("uuid");
+const port = process.env.PROT || 3051;
+
+console.clear();
 
 let takenNames = [];
 let connectedUsers = [];
-let currentClient;
-let otherUsers = [];
-let userCounter = 0;
+let superSecretPassword = "password";
 
 class Client {
-  constructor(name, id, socket) {
+  constructor(name, socket) {
     this.name = name;
-    this.id = id;
+    this.id = uuidv4();
     this.socket = socket;
+    this.adminAttempts = 0;
+    this.admin = false;
   }
 }
-
-function idGenerator() {
-  let result = Math.floor(Math.random() * 100000);
-  return result;
-}
-function changeName(newName) {
-  checkForDuplicateNames(newName);
-  sendMessage(`${currentClient.name} has changed their name to ${newName}`);
-  console.log(`--- ${currentClient.name} Changed their name to ${name}`);
-  takenNames.push(newName);
-}
-
-function checkForDuplicateNames(name) {
-  takenNames.filter((element) => {
-    if (element.includes(name) && element.length === name.length) {
-      name += "(1)";
-    }
-  });
-  return name;
-}
-function getOtherUsers(client) {
-   
-  connectedUsers.filter((user) => {
-    if (user.name !== currentClient.name)
-      otherUsers.push(user);
-  });
-  return otherUsers;
-}
-
-function whisperUser(user, message) {
-  if (checkConnectedUsers(user))
-    username.socket.write(`(*)${currentClient.name}: ${message}`);
-}
-console.clear();
 //Server Starts Here
 const server = net
   .createServer((socket) => {
-    userCounter++;
-    currentClient = new Client(`Guest`, idGenerator(), socket);
-    currentClient.name = `Guest${id /10000}`
-    connectedUsers.push(currentClient);
-    getOtherUsers(currentClient);
-      console.log(`User ${currentClient.name} connected`);
-      currentClient.socket.write('Welcome to the server!') 
-      serverLog.write(`User ${currentClient.name} connected ${new Date().toISOString()}\n`)
+    connectedUsers.push(
+      new Client(`Guest${Math.floor(Math.random() * 1000)}`, socket)
+    );
+    let currentClient = checkConnectedUsers(socket);
+    if (connectedUsers.length === 1) currentClient.admin = true;
+    takenNames.push(currentClient.name);
+    console.log(`User ${currentClient.name} connected`);
+    currentClient.socket.write(
+      `${colorSelector("green")}Welcome to the server ${currentClient.name}!`
+    );
+    sendBroadcast(
+      `User ${currentClient.name} has joined the server!`,
+      currentClient,
+      "green"
+    );
+    serverLog.write(
+      `User ${currentClient.name} connected ${new Date().toISOString()}\n`
+    );
+    
     socket.on("data", (data) => {
       checkForCommand(data.toString().trim());
     });
+
     socket.on("end", () => {
+      let currentUser = checkConnectedUsers(socket);
       connectedUsers.filter((user, index) => {
-        if (user.name === currentClient.name) {
+        if (user.name === currentUser.name) {
           console.log(`${user.name} disconnected`);
           serverLog.write(
             `${user.name} Disconnected: ${new Date().toISOString()}`
           );
+          sendBroadcast(
+            `User ${user.name} disconnected`,
+            currentUser,
+            "yellow"
+          );
+          takenNames.splice(takenNames.indexOf(user.name), 1);
           connectedUsers.splice(index, 1);
         }
       });
     });
 
     //functions
-    function checkConnectedUsers(username) {
-      return connectedUsers.filter((user) => {
-        if (user.name === username) return true;
+    function checkConnectedUsers(property) {
+      let result;
+      connectedUsers.filter((user) => {
+        if (
+          user.name == property ||
+          user.id == property ||
+          user.socket == property
+        ) {
+          result = user;
+        }
       });
+      return result;
     }
 
-    function runCommand(command, nameToCheck, input) {
-      let client;
-      if (checkCOnnectedUsers(nameToCheck)) {
-        client = connectedUsers.filter((user) => user.name == nameToCheck);
-      } else {
-      }
-      let response = client.socket.write();
-      console.log(
-        `---COMMAND: ${command} username: ${client.name} INPUT ${input}`
-      );
-      if (command.match(/^\/w$/)) {
-        whisperUser(client.name, input);
+    function runCommand(command, nameToCheck, input, sendingUser) {
+      let client = checkConnectedUsers(nameToCheck);
+      if (command.match(/^\/[\?]$/)) {
+        let help = [
+          `Available commands:`,
+          "/w: Sends a private message to a user in the server.",
+          "-Syntax: /w <username> <message>",
+          "/username: Change your username.",
+          "-Syntax: /username <newusername>",
+          "/kick: Removes a user from the server if admin password is used.",
+          "-Syntax: /kick <username> <password>",
+          "/clientlist: shows a list of all currently connected users.",
+          "Names with the * symbol are admin",
+          "-Syntax: /clientlist",
+        ];
+        help.forEach((line) => {
+          sendingUser.socket.write(`${colorSelector("yellow")}${line}\n`);
+        });
       } else if (command.match(/^\/username/)) {
-        if (
-          username.length <= 10 &&
-          username.match(/^[A-Za-z0-9]+[A-Za-z0-9]*$/g)
-        ) {
-          changeName(client);
-          response(`your username is now changed to ${username}`);
+        if (nameToCheck) {
+          if (
+            nameToCheck.length <= 10 &&
+            nameToCheck.match(/^[A-Za-z0-9]+[A-Za-z0-9]*$/g)
+          ) {
+            changeName(nameToCheck, sendingUser);
+          } else {
+            sendingUser.socket.write(
+              `${colorSelector(
+                "red"
+              )}"${nameToCheck}" is an invalid username, Please use only alphanumeric characters`
+            );
+          }
         } else {
-          response(
-            `"${username}" is an invalid username, Please use only alphanumeric characters`
+          sendingUser.socket.write(
+            `${colorSelector("red")}Please enter a desired username`
           );
         }
+      } else if (command.match(/^\/w$/)) {
+        if (client) {
+          if (nameToCheck && input) {
+            client.socket.write(
+              `${colorSelector("cyan")}${sendingUser.name} whispered: ${input}`
+            );
+          } else if (nameToCheck) {
+            sendingUser.socket.write(
+              `${colorSelector(
+                "red"
+              )}No message entered, try /? for more information`
+            );
+          } else {
+            sendingUser.socket.write(
+              `${colorSelector(
+                "red"
+              )}No username entered. try /? for more information`
+            );
+          }
+        }
       } else if (command.match(/^\/kick/)) {
-        if (checkConnectedUsers(username) && input === superSecretPassword) {
-          sendMessage(`${username} has been removed from the server`);
+        if (client && sendingUser.admin) {
+          if (sendingUser.adminAttempts > 3) {
+            sendingUser.socket.write(
+              `${colorSelector("red")}Too many failed attempts. try again later`
+            );
+            setTimeout(() => {
+              if (socket) {
+                let user = checkConnectedUsers(socket);
+                user.adminAttempts = 0;
+              }
+            }, 60000);
+          } else {
+            if (
+              input === superSecretPassword &&
+              client.name != sendingUser.name
+            ) {
+              connectedUsers.filter((user, index) => {
+                if (user.name === client.name) {
+                  console.log(`${user.name} has been removed from the server`);
+                  serverLog.write(
+                    `${
+                      user.name
+                    } has been removed from the server: ${new Date().toISOString()}`
+                  );
+                  user.socket.write(
+                    `${colorSelector(
+                      "magenta"
+                    )}You have been removed from the server`
+                  );
+                  sendBroadcast(
+                    `${user.name} has been removed from the server`,
+                    sendingUser,
+                    "magenta"
+                  );
+                  user.socket.end();
+                }
+              });
+            } else if (input !== superSecretPassword) {
+              sendingUser.socket.write(
+                `${colorSelector(
+                  "red"
+                )}Incorrect admin password, please try again`
+              );
+              sendingUser.adminAttempts++;
+            } else if (client.name === sendingUser.name) {
+              sendingUser.socket.write(
+                `${colorSelector(
+                  "red"
+                )}You cannot remove yourself. To end the session press CONTROL + C`
+              );
+            }
+          }
         } else {
-          response(`${username} doesn't exist`);
+          sendingUser.socket.write(
+            `${colorSelector(
+              "red"
+            )}Admin privleges required to use this command.`
+          );
+        }
+      } else if (command.match(/^\/addAdmin/)) {
+        if (client && input === superSecretPassword) {
+          client.admin = true;
+          client.name == sendingUser.name
+            ? sendingUser.socket.write(
+                `${colorSelector("magenta")}You are now admin`
+              )
+            : client.socket.write(
+                `${colorSelector("magenta")}You are now an admin`
+              ),
+            sendingUser.socket.write(
+              `${colorSelector("magenta")}You have granted ${
+                client.name
+              }  admin privileges`
+            );
+        } else if (client && input !== superSecretPassword) {
+          sendingUser.adminAttempts++;
+          sendingUser.socket.write(
+            `${colorSelector("red")}Incorrect admin password, please try again`
+          );
+        } else {
+          sendingUser.socket.write(
+            `${colorSelector("red")}Please enter a valid name`
+          );
         }
       } else if (command.match(/^\/clientlist/)) {
-        let usernames = connectedUsers.map((user) => {
-          usernames.push(user.name);
+        let usernames = [];
+        connectedUsers.forEach((user) => {
+          user.admin && user.name == sendingUser.name
+            ? usernames.push(`*(${user.name})*`)
+            : user.name === sendingUser.name
+            ? usernames.push(`(${user.name})`)
+            : user.admin
+            ? usernames.push(`*${user.name}*`)
+            : usernames.push(user.name);
         });
-        response(`Currently connected users:\r\n${usernames.join("\n")}`);
-      } else if (command.match(/^\/[\?]$/)) {
-        let help = [
-          "Available commands:",
-          "/w:Sends a private message to a user in the server.",
-          "Syntax: /w <username> <message>",
-          "/username: Change your username.",
-          "Syntax: /username <newusername>",
-          " /kick: Removes a user from the server if admin password is used.",
-          "Syntax: /kick <username> <password>",
-          "/clientlist: shows a list of all currently connected users. Syntax: /clientlist",
-        ];
-        help.map((line) => {
-          socket.write(line);
-        });
+        sendingUser.socket.write(
+          `${colorSelector(
+            "yellow"
+          )}Currently connected users:\r\n${usernames.join("\n")}`
+        );
+      } else {
+        sendingUser.socket.write(
+          `${colorSelector(
+            "red"
+          )}${command} is not a valid command. type /? for a list of commands`
+        );
       }
-      return response;
     }
 
-    function cleanInput(input) {
+    function cleanInput(input, user) {
       let seperatedInput = input.split(" ");
       return runCommand(
         seperatedInput[0],
         seperatedInput[1],
-        seperatedInput.slice(2).join(" ")
+        seperatedInput.slice(2).join(" "),
+        user
       );
     }
-    function sendMessage(message) {
-      otherUsers.forEach((user) => {
-        user.socket.write(`${currentClient.name}: ${message}`);
+
+    function sendBroadcast(message, sendingUser, color) {
+      connectedUsers.forEach((user) => {
+        if (user.name !== sendingUser.name) {
+          user.socket.write(`${colorSelector(color)}${message}`);
+          serverLog.write(`${message}  | ${new Date().toISOString()}\n`);
+        }
+      });
+    }
+
+    function sendMessage(message, sendingUser) {
+      connectedUsers.forEach((user) => {
+        if (user.id !== sendingUser.id && sendingUser.admin) {
+          user.socket.write(
+            `${colorSelector("magenta")}${sendingUser.name}: ${colorSelector(
+              "blue"
+            )}${message}`
+          );
+          chatLog.write(
+            `${currentClient.name}: ${message} | ${new Date().toISOString()}\n`
+          );
+        } else if (user.id !== sendingUser.id) {
+          user.socket.write(
+            `${colorSelector("blue")}${sendingUser.name}: ${message}`
+          );
+          chatLog.write(
+            `${currentClient.name}: ${message} | ${new Date().toISOString()}\n`
+          );
+        }
       });
     }
 
     function checkForCommand(input) {
+      let sendingUser = checkConnectedUsers(socket);
       if (input.match(/^\//)) {
-        return cleanInput(input);
+        return cleanInput(input, sendingUser);
       } else {
-        sendMessage(input);
-        chatLog.write(`${input} ${new Date().toISOString()}\n`);
+        sendMessage(input, sendingUser);
       }
     }
+
+    function changeName(desiredName, currentUser) {
+      if (takenNames.includes(desiredName)) {
+        currentUser.socket.write(
+          `${colorSelector(
+            "red"
+          )}the username "${desiredName}" is already taken please try a different one`
+        );
+      } else {
+        sendBroadcast(`name changed to ${desiredName}`, currentUser, "green");
+        currentUser.socket.write(
+          `${colorSelector(
+            "green"
+          )}your username is now changed to ${desiredName}`
+        );
+        console.log(`${currentUser.name} Changed their name to ${desiredName}`);
+        currentUser.name = desiredName;
+        takenNames.push(desiredName);
+      }
+    }
+
+    function colorSelector(color) {
+      let result = "";
+      switch (color) {
+        case "blue":
+          result = "\\x1b[34m";
+          break;
+        case "red":
+          result = "\\x1b[31m";
+          break;
+        case "green":
+          result = "\\x1b[32m";
+          break;
+        case "yellow":
+          result = "\\x1b[33m";
+          break;
+        case "magenta":
+          result = "\\x1b[35m";
+          break;
+        case "cyan":
+          result = "\\x1b[36m";
+          break;
+        default:
+          result = "\\x1b[37m";
+      }
+      return result;
+    }
   })
-  .listen(3050, () => {
+  .listen(port, () => {
     serverLog.write(`Server started: ${new Date().toISOString()}\n`);
-    console.log("---server is up");
+    console.log(`Server is listening on ${port}`);
   });
 
 server.on("close", () => {
-  serverLog.write(`Server started: ${new Date().toISOString()}\n`);
+  serverLog.write(`Server ended: ${new Date().toISOString()}\n`);
 });
